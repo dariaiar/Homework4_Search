f75e85d321b3c19753d0a5ff2c1f5e92b9c58038
Daria Iaroshchuk <daria.iaroshchuk@gmail.com>
2024-05-29 13:12:03 +0300

    add separate functions

--- homework4_search.go
+++ homework4_search.go
@@ -4,8 +4,23 @@ import "fmt"
 
 func main() {
 
-	var shoppingList []string = make([]string, 5)
-	for i := 0; i < 5; i++ {
+	const numberOfProductsInList = 4
+
+	shoppingList := createShoppingList(numberOfProductsInList)
+
+	fmt.Println("Your shopping list is:", shoppingList)
+
+	searchProduct(numberOfProductsInList, shoppingList)
+
+	printRangeShoppingList(shoppingList)
+
+}
+
+func createShoppingList(m int) []string {
+
+	var shoppingList []string = make([]string, m)
+
+	for i := 0; i < m; i++ {
 		fmt.Printf("Here is your shopping list. Input product #%v  -  ", i)
 		var product string
 		fmt.Scan(&product)
@@ -13,18 +28,28 @@ func main() {
 		shoppingList[i] = product
 
 	}
-	fmt.Println("Your shopping list is:", shoppingList)
+
+	return shoppingList
+
+}
+
+func searchProduct(numberOfProductsInList int, shoppingList []string) {
 
 	var findWord string
 	fmt.Printf("To search, type name of the product:   ")
 	fmt.Scan(&findWord)
 
-	for n := 0; n < 5; n++ {
+	for n := 0; n < numberOfProductsInList; n++ {
 		if findWord == shoppingList[n] {
 			fmt.Printf("FOUND MATCH -- product #%v,  %v   \n", n, shoppingList[n])
-			//} else {
-			//	fmt.Println("MATCH was not found")
+		} else {
+			fmt.Println("MATCH was not found")
 		}
 	}
+}
 
+func printRangeShoppingList(shoppingList []string) {
+	for b, rangedShoppingList := range shoppingList {
+		fmt.Println(b, rangedShoppingList)
+	}
 }
